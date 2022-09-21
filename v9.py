6685b64d053d80a866ed93463842cd8eb0f1b7b0
tarynbytes <62620959+tarynbytes@users.noreply.github.com>
2022-09-21 09:28:51 -0400

    Update v9.py

--- v9.py
+++ v9.py
@@ -46,8 +46,38 @@ class Log():
 
 
 
+class User():
+    """User objects are groups of sessions associated with a particular userid."""
+    def __init__(self, sessions, userid):
+        self._sessions = sessions
+        self._userid = userid
+        self._starts = []
+    
+    def __str__(self):
+        ret_str = f"USERID: {self._userid}"
+        for session in self._sessions:
+            ret_str += f"\n{session}"
+        return ret_str
+
+    def get_starts(self):
+        for session in self._sessions:
+            for log in session._session_logs:
+                if 1 == log.status:
+                    self._starts.append(log)
+
+    @property
+    def sessions(self):
+        return self._sessions
+    @property
+    def id(self):
+        return self._userid
+    @property
+    def starts(self):
+        return self._starts
+
+
 class Session():
-    """ Session objects are defined as a group of logs with the same userid, url, and tabid. """
+    """Session objects are defined as a group of logs with the same userid, url, and tabid."""
     def __init__(self, session_id):
         self._session_id = session_id
         self._session_logs = []
@@ -58,7 +88,7 @@ class Session():
         self._downloads = []
 
     def sort_logs(self):
-        self._session_logs = sorted(self._session_logs, key=lambda val: val.timestamp)
+        self._session_logs = sorted(self._session_logs, key=lambda val: (val.userid, val.url, val.tabid, val.timestamp))
         self._start = self._session_logs[0].timestamp
 
     def add_log(self, log):
@@ -78,7 +108,7 @@ class Session():
                     continue
 
     def __str__(self):
-        ret_str = f"SESSION HASH:\t{self._session_id}"
+        ret_str = f"\tSESSION HASH:{self._session_id}"
         for log in self._session_logs:
             ret_str += f"\n\t{log.__str__()}"
 
@@ -104,7 +134,7 @@ class Session():
 
 
 class Download():
-    """Associates Downloads to each Session object. """
+    """Associates Downloads to each Session object."""
     def __init__(self, session, start, end):
         self._session = session
         self._start = start
@@ -132,58 +162,44 @@ class Download():
 
 
 class Analyzer():
-    """Performs analysis operations on sessions."""
-    def __init__(self, sessions):
-        self._sessions = sessions
+    """Performs analysis operations on [sessions corresponding to] Users."""
+    def __init__(self, users):
+        self._users = users
     
-    def timeline(self, print_sorted):
+    def timeline(self, users):
         """ Prints a chronological, intelligible sequence of events of the whole log file for reference."""
         print("################################### TIMELINE #######################################")
-        for key, value in print_sorted.items():
-            print(f"\nUSERID: {key}")
-            for val in value:
-                print(f"{val}")
-
-    def avg_url_dload_time(self, sessions):
+        for user in users:
+            print(user)
+       
+    def avg_url_dload_time(self, users):
         """Determines the average download time for each session for each user.
             This information can indicate connectivity speed per user."""
         print("\n####################### AVERAGE DOWNLOAD TIME PER USER ##############################")
         avg_times = collections.defaultdict(list)
-        for session in sessions:
-            download_times = []
-            for download in session.downloads:
-                download_times.append(download.end - download.start)
-            if download_times:
-                avg_times[session.userid].append(f"URL: {session.url}, TABID: {session.tabid}, Average download time: {sum(download_times) / len(download_times)}ms")
-        
-        for key, value in avg_times.items():
-            print(f"\nUSERID: {key}")
-            for val in value:
-                print(f"{val}")
-        
+        for user in users:
+            for session in user.sessions:
+                download_times = []
+                for download in session.downloads:
+                    download_times.append(download.end - download.start)
+                if download_times:
+                    avg_times[user.id].append(f"URL: {session.url}, TABID: {session.tabid}, Average download time: {sum(download_times) / len(download_times)}ms")
+        self.print_results(avg_times)
 
-    def avg_session_overlap(self, sessions_sorted):
+    def avg_session_overlap(self, users):
         """Determines the average overlap time among sessions per user.
             Overlap defined as where one website is still downloading while a second one starts downloading.
             This information can indicate how frequent a user is to open new tabs."""
         print("\n########################## AVERAGE OVERLAP PER USER #################################")
-        for userid, sessions in sessions_sorted.items():
-            starts = []
-            for session in sessions:
-                for log in session.logs:
-                    if 1 == log.status:
-                        starts.append(log)
+        for user in users:
             overlaps = collections.defaultdict(list)
-            for session in sessions:
+            for session in user.sessions:
                 for download in session.downloads:
-                    for start in starts:
+                    for start in user.starts:
                         if download.start < start.timestamp < download.end and start.url != session.url:
-                            overlaps[session.userid].append(f"\n  [!] Overlap!\n\tSTART:   {download.start}\t{session.url}\n\tOVERLAP: {start.timestamp}\t{start.url}\n\tEND:     {download.end}\t{session.url}")
-            for key, value in overlaps.items():
-                print(f"\nUSERID: {key}")
-                for val in value:
-                    print(f"{val}")
-                    
+                            overlaps[user.id].append(f"\n  [!] Overlap!\n\tSTART:   {download.start}\t{session.url}\n\tOVERLAP: {start.timestamp}\t{start.url}\n\tEND:     {download.end}\t{session.url}")
+            self.print_results(overlaps)
+ 
 
     def avg_dload_time_before_overlap(self):
         """Determines the average time that a website starts downloading before an overlap happens."""
@@ -193,40 +209,48 @@ class Analyzer():
         """Determines how many websites there are in each user's overlaps."""
         pass
 
-
     def more(self):
         """what else?"""
         pass
+    
+    def print_results(self, dct):
+        for key, value in dct.items():
+            print(f"\nUSERID: {key}")
+            for val in value:
+                print(f"{val}")
 
-
-
-def generate_sessions(logs):
-    """ Returns:
-            sessions -> a list of session objects
-            sessions_sorted -> a dictionary of {userid : [session_objects_list]}
-            print_sorted -> a dictionary of {userid : "string of logs"}
-    """
+def generate_user_sessions(logs):
+    """ Initializes Session objects and returns a list of User objects [corresponding to Sessions]."""
     sessions = {}
-    sessions_sorted = collections.defaultdict(list)
-    print_sorted = collections.defaultdict(list)
+    users = []
 
     for log in logs:
+        if log.userid not in users:
+            users.append(log.userid)
         try:
             sessions[log.session_hash].add_log(log)
         except Exception as e:
             sessions[log.session_hash] = Session(log.session_hash)
             sessions[log.session_hash].add_log(log)
+
     sessions = list(sessions.values())
     for session in sessions:
         session.sort_logs()
         session.get_downloads()
-        str = f"\tHASH: {session.hash}"
-        for log in session._session_logs:
-            str += f"\n{log}"
-        sessions_sorted[session.userid].append(session)
-        print_sorted[session.userid].append(str)
 
-    return sorted(sessions, key=lambda session: (session.start, session.userid, session.url, session.tabid)), sessions_sorted, print_sorted
+    user_dct = {}
+    for user in users:
+        session_lst = []
+        for session in sessions:
+            if user == session.userid:
+                session_lst.append(session)
+        user_dct[user] = User(session_lst, user)
+    users = list(user_dct.values())
+
+    for user in users:
+        user.get_starts()
+
+    return users
 
 def parse_logs(filename:str):
     """Returns a list of parsed log_str objects, where each log_str object is a list of five strings."""
@@ -244,12 +268,12 @@ def handle_args():
 def main() -> int:
     args = handle_args()
     logs = parse_logs(args.filename)
-    sessions, sessions_sorted, print_sorted = generate_sessions(logs) # returns a list of Session objects and a dictionary of {userid:session_objects_list} respectively.
-
-    analyzer = Analyzer(sessions)
-    #analyzer.timeline(print_sorted)
-    #analyzer.avg_url_dload_time(sessions)
-    analyzer.avg_session_overlap(sessions_sorted)
+    users = generate_user_sessions(logs)
+    analyzer = Analyzer(users)
+    #analyzer.timeline(users)
+    #analyzer.avg_url_dload_time(users)
+    #analyzer.avg_session_overlap(users)
+    
 
     return 0
 
